95f69cb096b8e9a015d9d82d101bb8e3d3a9524f
guigzzz <guillaume.ws.rame@gmail.com>
2023-05-14 09:19:35 +0100

    automated setup

--- src/configuration.rs
+++ src/configuration.rs
@@ -8,11 +8,10 @@ use spacedust::apis::configuration::Configuration;
 
 use crate::limiter::RateLimiter;
 
-lazy_static! {
-    // having a static singleton configuration means that the throttling will apply to any client we instantiate
-    pub static ref CONFIGURATION: Configuration = {
-        dotenv().ok();
+pub struct ConfigurationFactory {}
 
+impl ConfigurationFactory {
+    pub fn get_config(token: &str) -> Configuration {
         let retry_policy = ExponentialBackoff::builder().build_with_max_retries(3);
 
         let client = reqwest_middleware::ClientBuilder::new(ClientBuilder::new().build().unwrap())
@@ -21,9 +20,17 @@ lazy_static! {
             .build();
 
         Configuration {
-            bearer_access_token: Some(env::var("TOKEN").unwrap()),
+            bearer_access_token: Some(token.to_owned()),
             client,
             ..Default::default()
         }
+    }
+}
+
+lazy_static! {
+    // having a static singleton configuration means that the throttling will apply to any client we instantiate
+    pub static ref CONFIGURATION: Configuration = {
+        dotenv().ok();
+        ConfigurationFactory::get_config(env::var("TOKEN").unwrap().as_str())
     };
 }
--- src/setup.rs
+++ src/setup.rs
@@ -1,14 +1,197 @@
-use crate::configuration::CONFIGURATION;
+use std::time::Duration;
 
+use chrono::DateTime;
+use log::info;
+use spacedust::{
+    apis::{
+        configuration::Configuration, contracts_api, default_api::register, fleet_api, systems_api,
+    },
+    models::{
+        waypoint_trait::Symbol, NavigateShipRequest, PurchaseShipRequest, RegisterRequest,
+        WaypointType,
+    },
+};
 
+use crate::configuration::ConfigurationFactory;
 
 pub struct Setup {}
 
 impl Setup {
+    pub async fn setup_account(username: &str) {
+        info!("[SETUP] Registering");
+        let agent = register(
+            &Configuration::new(),
+            Some(RegisterRequest::new(
+                spacedust::models::register_request::Faction::Cosmic,
+                username.to_string(),
+            )),
+        )
+        .await
+        .unwrap();
 
-    pub fn setup_account() {
+        let token = agent.data.token.as_str();
+        info!("[SETUP] TOKEN={token}");
 
-        let configuration = &CONFIGURATION;
+        let configuration = &ConfigurationFactory::get_config(agent.data.token.as_str());
 
+        let contract_ids: Vec<_> = contracts_api::get_contracts(configuration, None, None)
+            .await
+            .unwrap()
+            .data
+            .iter()
+            .map(|c| c.id.to_owned())
+            .collect();
+        info!("[SETUP] Found {} contracts", contract_ids.len());
+
+        for id in contract_ids {
+            info!("[SETUP] Accepting contract {}", id);
+            contracts_api::accept_contract(configuration, id.as_str(), 0)
+                .await
+                .unwrap();
+        }
+
+        let ships = fleet_api::get_my_ships(configuration, None, None)
+            .await
+            .unwrap()
+            .data;
+        info!(
+            "Ships: {}",
+            ships
+                .iter()
+                .map(|s| s.symbol.to_owned())
+                .collect::<Vec<_>>()
+                .join(", ")
+        );
+
+        let ship = fleet_api::get_my_ship(
+            configuration,
+            format!("{}-1", username.to_ascii_uppercase()).as_str(),
+        )
+        .await
+        .unwrap()
+        .data;
+
+        let system = ship.nav.system_symbol.as_str();
+        info!("[SETUP] Home system is {system}");
+
+        let waypoints = systems_api::get_system_waypoints(configuration, system, None, None)
+            .await
+            .unwrap()
+            .data;
+
+        let shipyard = waypoints
+            .iter()
+            .find(|w| w.traits.iter().any(|t| t.symbol == Symbol::Shipyard))
+            .unwrap();
+        info!("[SETUP] Found shipyard: {}", shipyard.symbol);
+
+        loop {
+            info!("[SETUP] Buying ship");
+
+            let purchase = fleet_api::purchase_ship(
+                configuration,
+                Some(PurchaseShipRequest::new(
+                    spacedust::models::ShipType::MiningDrone,
+                    shipyard.symbol.to_owned(),
+                )),
+            )
+            .await;
+
+            match purchase {
+                Result::Ok(_) => continue,
+                Result::Err(e) => {
+                    info!("[SETUP] Failed to buy ship, assuming out of money");
+                    dbg!(e);
+                    break;
+                }
+            }
+        }
+
+        let asteroid_field = waypoints
+            .iter()
+            .find(|w| w.r#type == WaypointType::AsteroidField)
+            .unwrap();
+
+        info!("[SETUP] Found asteroid field: {}", asteroid_field.symbol);
+
+        let ships = fleet_api::get_my_ships(configuration, None, None)
+            .await
+            .unwrap()
+            .data;
+
+        if ships.len() == 1 {
+            panic!("We only have one ship ? Did we fail to buy any ?")
+        }
+
+        let mut wait_seconds = None;
+        for s in ships {
+            info!(
+                "[SETUP] Navigating {} to {}",
+                s.symbol, asteroid_field.symbol
+            );
+            let nav = fleet_api::navigate_ship(
+                configuration,
+                s.symbol.as_str(),
+                Some(NavigateShipRequest::new(asteroid_field.symbol.to_string())),
+            )
+            .await
+            .unwrap()
+            .data;
+
+            let route = nav.nav.route;
+            let departure = DateTime::parse_from_rfc3339(route.departure_time.as_str()).unwrap();
+            let arrival = DateTime::parse_from_rfc3339(route.arrival.as_str()).unwrap();
+
+            let eta = (arrival - departure).num_seconds() as u64;
+
+            wait_seconds = wait_seconds.map_or(Some(eta), |v| Some(u64::max(v, eta)));
+        }
+
+        let wait = wait_seconds.unwrap();
+        info!("[SETUP] Ships travelling to asteroid field, waiting {wait} seconds");
+        tokio::time::sleep(Duration::from_secs(wait)).await;
+
+        info!("[SETUP] Ready to go!");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::{distributions::Alphanumeric, Rng};
+
+    #[tokio::test]
+    async fn test_setup() {
+        init_logging();
+
+        let user = random_string();
+
+        info!("user: {}", user);
+
+        Setup::setup_account(user.as_str()).await
+    }
+
+    fn random_string() -> String {
+        rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(7)
+            .map(char::from)
+            .collect()
+    }
+
+    fn init_logging() {
+        fern::Dispatch::new()
+            .format(|out, message, record| {
+                out.finish(format_args!(
+                    "[{} {}] {}",
+                    humantime::format_rfc3339(std::time::SystemTime::now()),
+                    record.level(),
+                    message
+                ))
+            })
+            .level(log::LevelFilter::Info)
+            .chain(std::io::stdout())
+            .apply()
+            .unwrap();
     }
 }
